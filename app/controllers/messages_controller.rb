6de1bf3c365df1403d60f0ce89e45e266d8b69f4
Ryan Lapeyre <ryanlapeyre@gmail.com>
2018-01-09 02:21:57 -0600

    fixed messaging locally

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,17 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
-end
+  before_action :set_chatroom
 
 def create
     message = @chatroom.messages.new(message_params)
     message.user = current_user
-
     message.save
     redirect_to @chatroom
-
 end
-
-
-
 private
-
     def set_chatroom
         @chatroom = Chatroom.find(params[:chatroom_id])
     end
-
-
     def message_params
         params.require(:message).permit(:body)
     end
