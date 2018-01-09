ffcd0af7cccc1620d747e4bee0164fe1cf5c4fd1
Ryan Lapeyre <ryanlapeyre@gmail.com>
2018-01-09 02:15:55 -0600

    added messages_controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,26 @@
+class MessagesController < ApplicationController
+  before_action :authenticate_user!
+end
+
+def create
+    message = @chatroom.messages.new(message_params)
+    message.user = current_user
+
+    message.save
+    redirect_to @chatroom
+
+end
+
+
+
+private
+
+    def set_chatroom
+        @chatroom = Chatroom.find(params[:chatroom_id])
+    end
+
+
+    def message_params
+        params.require(:message).permit(:body)
+    end
+end
