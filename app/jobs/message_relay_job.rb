d0d6c1e7e616a453ba21bb041d34aa406d126afe
Ryan Lapeyre <ryanlapeyre@gmail.com>
2018-01-14 16:56:23 -0600

    finished message relay job, saved file. changed view for chatrooms

--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -2,7 +2,9 @@ class MessageRelayJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}" ,
-    message: MessageController.render(message)
+    ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}" , {
+    message: MessageController.render(message),
+    chatroom_id: message.chatroom.id
+  }
   end
 end
