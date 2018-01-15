d169c53070238df847d4259002cb8e09b68dcb3e
Ryan Lapeyre <ryanlapeyre@gmail.com>
2018-01-14 20:43:27 -0600

    Hopefully heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://chatter-pear.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://chatter-pear.herokuapp.com', /http:\/\/chatter-pear.herokuapp.com ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -89,8 +89,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.web_socket_server_url = "wss://chatter-pear.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https:chatter-pear.herokuapp.com', 'http://chatter-pear.herokuapp.com']
+#  config.web_socket_server_url = "wss://chatter-pear.herokuapp.com/cable"
+#  config.action_cable.allowed_request_origins = ['https:chatter-pear.herokuapp.com', 'http://chatter-pear.herokuapp.com']
 
 
 end
